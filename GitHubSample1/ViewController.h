eccc498f6a8aca7bc1148b45197c682e1b4f556d
Student <student@achiever132.local>
2017-01-17 02:13:00 -0800

    Initial Commit

---
+++ GitHubSample1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubSample1
+//
+//  Created by Student on 17/01/17.
+//  Copyright © 2017 Emp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitHubSample1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitHubSample1
+//
+//  Created by Student on 17/01/17.
+//  Copyright © 2017 Emp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
